95d20254b20941cfff3e10c9c49a3c028f26b1b0
olaysco <olayiwolaodunsi@gmail.com>
2021-02-20 01:26:07 +0100

    display celebrants for the month on the main page:

--- web-client/src/components/MonthCelebrant.vue
+++ web-client/src/components/MonthCelebrant.vue
@@ -2,23 +2,34 @@
 	<div>
 		<div class="flex flex-col">
 			<h4 class="text-gray-500 mb-2">{{ currentMonth}} Celebrants</h4>
-			<contact-table :list="contact" :showOperation="true" />
+			<contact-table :list="monthCelebrant" :showOperation="true" />
 		</div>
 	</div>
 </template>
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Vue, Watch } from "vue-property-decorator";
 import ApiContact from "../api/contact";
+import { mapState } from "vuex";
 // eslint-disable-next-line no-unused-vars
 import { Contact } from "../types";
 import ContactTable from "./ContactTable.vue";
 
 @Component({
 	components: { ContactTable },
+	computed: mapState(["user", "contact"]),
 })
 export default class MonthCelebrant extends Vue {
-	public contact: Contact[] = [];
+	public monthCelebrant: Contact[] = [];
+	public contact!: Contact[];
 	public currentMonth: string = ApiContact.currentMonthName();
+
+	@Watch("contact", { immediate: true })
+	onContactChanged() {
+		const currentMonth: number = new Date().getMonth() + 1;
+		this.monthCelebrant = this.contact.filter((info: Contact) => {
+			return (info.monthOfBirth = currentMonth);
+		});
+	}
 }
 </script>
 <style scoped>
